158636f140f499d9499e51fe4a64be85eb3dcd4b
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2022-12-07 20:40:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+	
+int main(){
+	int a = 7;
+	printf("         ,r'\"%d\n",a);
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+}
