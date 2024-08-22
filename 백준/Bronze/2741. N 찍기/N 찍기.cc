188c42523cb6320b1cfca03940accf01cef9fb84
jjh725089 <114462635+jjh725089@users.noreply.github.com>
2024-08-22 16:32:36 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cout << i + 1 << "\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
