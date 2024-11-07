60a2b328ffca347e60622bb307008112187fcac0
jeonghyun <114462635+jjh725089@users.noreply.github.com>
2024-11-07 21:12:24 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -12,14 +12,15 @@ int main(void) {
 	fib[0][1] = 0;
 	fib[1][0] = 0;
 	fib[1][1] = 1;
+	for (int j = 2; j < 42; j++)
+	{
+		fib[j][0] = fib[j - 1][0] + fib[j - 2][0];
+		fib[j][1] = fib[j - 1][1] + fib[j - 2][1];
+	}
+
 	for (int i = 0; i < T; i++)
 	{
 		cin >> N;
-		for (int j = 2; j < N + 1; j++)
-		{
-			fib[j][0] = fib[j - 1][0] + fib[j - 2][0];
-			fib[j][1] = fib[j - 1][1] + fib[j - 2][1];
-		}
 		cout << fib[N][0] << " " << fib[N][1] << '\n';
 	}
 	return 0;
